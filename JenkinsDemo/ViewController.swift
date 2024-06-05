a01fd9b00f4231f51370bf516caec6aa0d54e176
Mert GÖKDUMAN <mert.gokduman@traderlands.com>
2024-06-05 21:09:23 +0300

    BG updated

--- JenkinsDemo/ViewController.swift
+++ JenkinsDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        view.backgroundColor = .red
+        view.backgroundColor = .purple
         print("Hello World - 3")
     }
 
