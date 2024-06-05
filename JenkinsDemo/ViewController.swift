657091735d48b8c936d1355edadc39916de9a32d
Mert GÖKDUMAN <mert.gokduman@traderlands.com>
2024-06-05 19:23:06 +0300

    Initial Commit

---
+++ JenkinsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JenkinsDemo
+//
+//  Created by Mert GÖKDUMAN on 5.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
